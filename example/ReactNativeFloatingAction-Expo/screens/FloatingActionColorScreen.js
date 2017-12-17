46dec538a380e11937ee211976623ca6ae0e2614
Gonzalo Martin Santome Osuna <gonzalo.santome@zalando.de>
2017-12-17 19:28:11 +0100

    new Expo examples

---
+++ example/ReactNativeFloatingAction-Expo/screens/FloatingActionColorScreen.js
@@ -0,0 +1,75 @@
+import React, { PureComponent } from 'react';
+import { View, SafeAreaView, StyleSheet, Alert } from 'react-native';
+import { FloatingAction } from 'react-native-floating-action';
+
+import Property from '../components/Property';
+
+class FloatingActionColorScreen extends PureComponent {
+  static navigationOptions = {
+    title: 'Right position'
+  };
+
+  render() {
+    const actions = [{
+      color: '#FF4B4B',
+      text: 'Accessibility',
+      icon: require('../images/ic_accessibility_white.png'),
+      name: 'bt_accessibility',
+      position: 2
+    }, {
+      color: '#FF4B4B',
+      text: 'Language',
+      icon: require('../images/ic_language_white.png'),
+      name: 'bt_language',
+      position: 1
+    }, {
+      color: '#FF4B4B',
+      text: 'Location',
+      icon: require('../images/ic_room_white.png'),
+      name: 'bt_room',
+      position: 3
+    }, {
+      color: '#FF4B4B',
+      text: 'Video',
+      icon: require('../images/ic_videocam_white.png'),
+      name: 'bt_videocam',
+      position: 4
+    }];
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
+          <Property
+            propertyName="buttonColor (main action)"
+            propertyValue="#FF4B4B"
+            defaultValue="#1253bc"
+          />
+          <Property
+            propertyName="color (sub actions)"
+            propertyValue="#FF4B4B"
+            defaultValue="#1253bc"
+          />
+          <FloatingAction
+            buttonColor="#FF4B4B"
+            actions={actions}
+            position="right"
+            onPressItem={
+              (name) => {
+                Alert.alert('Icon pressed', `the icon ${name} was pressed`);
+              }
+            }
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+});
+
+export default FloatingActionColorScreen;
---
+++ example/ReactNativeFloatingAction-Expo/screens/FloatingActionLeftScreen.js
@@ -0,0 +1,65 @@
+import React, { PureComponent } from 'react';
+import { View, SafeAreaView, StyleSheet, Alert } from 'react-native';
+import { FloatingAction } from 'react-native-floating-action';
+
+import Property from '../components/Property';
+
+class FloatingActionLeftScreen extends PureComponent {
+  static navigationOptions = {
+    title: 'Right position'
+  };
+
+  render() {
+    const actions = [{
+      text: 'Accessibility',
+      icon: require('../images/ic_accessibility_white.png'),
+      name: 'bt_accessibility',
+      position: 2
+    }, {
+      text: 'Language',
+      icon: require('../images/ic_language_white.png'),
+      name: 'bt_language',
+      position: 1
+    }, {
+      text: 'Location',
+      icon: require('../images/ic_room_white.png'),
+      name: 'bt_room',
+      position: 3
+    }, {
+      text: 'Video',
+      icon: require('../images/ic_videocam_white.png'),
+      name: 'bt_videocam',
+      position: 4
+    }];
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
+          <Property
+            propertyName="position"
+            propertyValue="left"
+            defaultValue="right"
+          />
+          <FloatingAction
+            position="left"
+            actions={actions}
+            onPressItem={
+              (name) => {
+                Alert.alert('Icon pressed', `the icon ${name} was pressed`);
+              }
+            }
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+});
+
+export default FloatingActionLeftScreen;
