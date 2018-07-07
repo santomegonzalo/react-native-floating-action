11f3b33b7333e223657a72f318d209e0a9265c51
Gonzalo Santome <gonzalo@iMac.local>
2018-07-07 11:58:22 +0200

    fix lint warnings and remove size feature for now

--- example/ReactNativeFloatingAction-Expo/rn-cli.config.js
+++ example/ReactNativeFloatingAction-Expo/rn-cli.config.js
@@ -4,7 +4,8 @@ module.exports = {
   extraNodeModules: {
     "react-native": path.resolve(__dirname, "node_modules/react-native"),
     "react": path.resolve(__dirname, "node_modules/react"),
-    "prop-types": path.resolve(__dirname, "node_modules/prop-types")
+    "prop-types": path.resolve(__dirname, "node_modules/prop-types"),
+    "react-native-floating-action": path.resolve(__dirname, "component")
   },
   getProjectRoots: () => [path.join(__dirname, "..", ".."), __dirname]
 };
