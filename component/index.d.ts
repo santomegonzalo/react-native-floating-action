eaf8c152d2eb085336e57d7325d88ca624ea73ba
Julian Pinzer <t-jupinz@microsoft.com>
2018-07-31 15:56:04 -0700

    Added typings definition, fixed it so that it works if there are no actions provided.

---
+++ component/index.d.ts
@@ -0,0 +1,42 @@
+// Type definitions for react-native-action
+// Project: items-view-mobile
+
+declare module 'react-native-floating-action' {
+    import { Component } from 'react';
+
+    type position = "right" | "left" | "center";
+
+    declare class FloatingAction extends Component<IFloatingActionProps> {
+    }
+
+    export interface IActionProps {
+        color?: string;
+        icon?: JSX.Element;
+        name: string;
+        text?: string;
+        textBackground?: string;
+        textColor?: string;
+        component?: () => void;
+    }
+
+    export interface IFloatingActionProps {
+        actions?: IActionProps[];
+        color?: string;
+        distanceToEdge?: number;
+        visible?: boolean;
+        overlayColor?: string;
+        position?: position;
+        overrideWithAction?: boolean;
+        floatingIcon?: JSX.Element;
+        showBackground?: boolean;
+        openOnMount?: boolean;
+        actionsPaddingTopBottom?: number;
+        iconHeight?: number;
+        iconWidth?: number;
+        listenKeyboard?: boolean;
+        dismissKeyboardOnPress?: boolean;
+        onPressItem?: () => void;
+        onPressMain?: () => void;
+    }
+
+}
