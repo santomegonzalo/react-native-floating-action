af749f4aa9e98656884512b754db40d105dc42f9
Luke Czyszczonik <lukasz.czyszczonik@gmail.com>
2019-02-09 09:14:51 +0100

    Add missing TS type

--- index.d.ts
+++ index.d.ts
@@ -1,41 +1,45 @@
 // Type definitions for react-native-floating-action
 
 declare module 'react-native-floating-action' {
-    import { Component } from 'react';
+  import { Component } from 'react';
 
-    type position = "right" | "left" | "center";
+  type position = 'right' | 'left' | 'center';
 
-    export class FloatingAction extends Component<IFloatingActionProps> {
-    }
+  export class FloatingAction extends Component<IFloatingActionProps> {}
 
-    export interface IActionProps {
-        color?: string;
-        icon?: JSX.Element;
-        name: string;
-        text?: string;
-        textBackground?: string;
-        textColor?: string;
-        component?: () => void;
-    }
+  export interface IActionProps {
+    color?: string;
+    icon?: JSX.Element;
+    name: string;
+    text?: string;
+    textBackground?: string;
+    textColor?: string;
+    textElevation?: number;
+    margin?: number;
+    component?: () => void;
+    render?: () => void;
+  }
 
-    export interface IFloatingActionProps {
-        actions?: IActionProps[];
-        color?: string;
-        distanceToEdge?: number;
-        visible?: boolean;
-        overlayColor?: string;
-        position?: position;
-        overrideWithAction?: boolean;
-        floatingIcon?: JSX.Element;
-        showBackground?: boolean;
-        openOnMount?: boolean;
-        actionsPaddingTopBottom?: number;
-        iconHeight?: number;
-        iconWidth?: number;
-        listenKeyboard?: boolean;
-        dismissKeyboardOnPress?: boolean;
-        onPressItem?: (name?: string) => void;
-        onPressMain?: () => void;
-    }
-
+  export interface IFloatingActionProps {
+    actions?: IActionProps[];
+    color?: string;
+    distanceToEdge?: number;
+    visible?: boolean;
+    overlayColor?: string;
+    position?: position;
+    overrideWithAction?: boolean;
+    floatingIcon?: JSX.Element;
+    showBackground?: boolean;
+    openOnMount?: boolean;
+    actionsPaddingTopBottom?: number;
+    iconHeight?: number;
+    iconWidth?: number;
+    listenKeyboard?: boolean;
+    dismissKeyboardOnPress?: boolean;
+    onPressItem?: (name?: string) => void;
+    onPressMain?: () => void;
+    onClose?: () => void;
+    onOpen?: () => void;
+    onStateChange?: () => void;
+  }
 }
